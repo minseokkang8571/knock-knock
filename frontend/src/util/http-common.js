c293fe945bc466de64de190fccd1eeb7bdc15d70
minseokkang8571 <kangminseok1001@gmail.com>
2020-10-04 23:20:41 +0900

    Add .env to hide private variable

--- frontend/src/util/http-common.js
+++ frontend/src/util/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // axios 객체 생성
 export default axios.create({
-  baseURL: 'http://127.0.0.1:4000',
+  baseURL: process.env.VUE_APP_BASE_URL,
   headers: {
     'Content-type': 'application/json'
   }
