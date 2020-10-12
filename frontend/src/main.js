706fce9f0a00d1926556140547b4d217a5294dfa
minseokkang8571 <kangminseok1001@gmail.com>
2020-10-12 11:24:23 +0900

    Reduce bootstrap-vue resource

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
 import VueDOMPurifyHTML from 'vue-dompurify-html'
 import store from './store'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 
-Vue.use(BootstrapVue)
+// Bootstrap-vue plugin start
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  ModalPlugin,
+  DropdownPlugin,
+  PaginationPlugin,
+  NavbarPlugin,
+  ButtonPlugin,
+  FormPlugin,
+  FormInputPlugin,
+  FormTextareaPlugin
+} from 'bootstrap-vue'
+
+Vue.use(ModalPlugin)
+Vue.use(DropdownPlugin)
+Vue.use(PaginationPlugin)
+Vue.use(NavbarPlugin)
+Vue.use(ButtonPlugin)
+Vue.use(FormPlugin)
+Vue.use(FormInputPlugin)
+Vue.use(FormTextareaPlugin)
+// Bootstrap-vue plugin end
+
 Vue.use(VueDOMPurifyHTML)
 
 Vue.config.productionTip = false
